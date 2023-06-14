756b8f7f407d3b84e66e92cfa061d3751207893f
Marshall <romane.marshall@accenture.com>
2023-06-14 10:11:17 -0400

    completed all tests and functionality

--- weather-api/js/weather.js
+++ weather-api/js/weather.js
@@ -1,4 +1,6 @@
+const { error } = require("console");
 const fetch = require("node-fetch")
+const input = require('readline-sync');
 // function used to consume API
 async function getLatAndLong(city, state) {
 //how to make an API call to get lattitude and longitude:
@@ -6,30 +8,61 @@ async function getLatAndLong(city, state) {
 
 const res = await fetch("https://nominatim.openstreetmap.org/search?city=" + city + "&state=" + state + "&countrycodes=us&limit=9&format=json");
 const jsonData = await res.json();
-let weatherArr = [];
+console.log(jsonData)
+if(jsonData.length > 0){
+let coordinatesArr = [];
 //access long and lat inside of json and place inside of variable for later use
 let lat = jsonData[0]['lat']
 let long = jsonData[0]['lon']
-weatherArr.push(lat, long)
+coordinatesArr.push(lat, long)
+console.log(coordinatesArr)
   //return array with lat and long variables in it
-  return weatherArr;
+  return coordinatesArr;
+} else {
+  return jsonData
+}
   
 }
 // getLatAndLong()
 
-async function getForecast(){
+async function getForecast(lat, long){
 //API URL for getting weather forecast using lat and lon
+console.log('in here')
+console.log(lat + long)
 
-//"https://api.open-meteo.com/v1/forecast?latitude=xxx&longitude=xxx&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York"
+let url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat +"&longitude=" + long + "&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York"
+ const res = await fetch(url)
+ 
+  console.log(url)
+  const jsonData = await res.json();
+  console.log(jsonData)
+ if( jsonData["error"] !== true ){
+  // for(let i = 0; i < jsonData["daily"].length; i++) {
+  let tempMax = jsonData["daily"]["temperature_2m_max"]
+  let tempMin = jsonData["daily"]["temperature_2m_min"]
+  let tempArray = [tempMax, tempMin]
+  console.log("if data is not error")
+ return tempArray
+  // }
 
-  //res["temperature_2m_min"] will give you array of lows for next week
+ } else if(jsonData["error"] === true){
+  console.log("if data error ")
+  // let errorMessage = jsonData["reason"]
+  return []
+ }
+}
 
-  
+async function main(){
+  let city = input.question("Input latitude of city: ")
+  let state = input.question("Input Longitude of city: ")
+
+  let latLongArray = await getLatAndLong(city, state)
+  console.log('this is lat array: ' + latLongArray);
+  let  forcastArrays = await getForecast(latLongArray[0], latLongArray[1])
+
+  console.log(forcastArrays)
+ 
 }
 
 
-  
-
-
-
-module.exports = {getLatAndLong};
+module.exports = {getLatAndLong, getForecast, main};
--- weather-api/spec/weather.spec.js
+++ weather-api/spec/weather.spec.js
@@ -1,28 +1,36 @@
+
 const Weather = require('../js/weather.js');
+// const { get } = require('http');
 
 describe("weather", () => {
     it("Receives a valid response when calling location API", async () => {
-        //test that when you call getLatAndLong with valid input that it returns a response that is not empty
          
         
         let weatherArr = await Weather.getLatAndLong('atlanta', 'georgia');
-         //expect t to not be empty or have text etc
+
             expect(weatherArr).toEqual(["33.7489924", "-84.3902644"])
-         //expect t['lat'] to be a number or something
+
          
        });
        
        it("Receives empty response when calling location API", async () => {
         //test that when you call getLatAndLong with invalid input that it returns a response that is empty
-       
+        let weatherArr = await Weather.getLatAndLong('atttlanta', 'georgia');
          //call it with something like attttlllanta thats not valid
+         expect(weatherArr).toEqual([])
        });
        
        it("Receives valid response when calling Weather API", async () => {
         //test that when you call getForecast with valid input that it returns a response that is not empty
+        let weatherArr = await Weather.getForecast("33.7489924", "-84.3902644")
+        expect(weatherArr.length).toEqual(2)
+        expect(weatherArr[0].length).toEqual(7)
+        expect(weatherArr[1].length).toEqual(7)
        });
        
        it("Receives empty response when calling Weather API", async () => {
         //test that when you call getForecast with invalid input that it returns a response that is empty
+        let weatherArr = await Weather.getForecast("m", "f")
+        expect(weatherArr.length).toEqual(0)
        });
 })
